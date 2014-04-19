2dcc88534b18a94038e4487c00684a8783dad5bf
Dean Efrati <deanefrati@gmail.com>
2014-04-19 13:51:53 -0600

    Added another message on the main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Dev Branch");
+		System.out.println("Another message from the dev branch");
 		FeatureOne one = new FeatureOne();
 	}
 }

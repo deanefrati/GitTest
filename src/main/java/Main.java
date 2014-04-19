42fe11f0d4c386e18f00e4825dff628e5047d7ef
Dean Efrati <deanefrati@gmail.com>
2014-04-19 17:28:58 -0600

    reverted merge of feature two

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
 		System.out.println("Dev Branch");
 		System.out.println("Another message from the dev branch");
 		FeatureOne one = new FeatureOne();
+		System.out.println("reverted merge of feature two");
 	}
 }
